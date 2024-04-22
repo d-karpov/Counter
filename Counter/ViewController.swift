2032a79311d841de76df41b333b419e6e8c9e542
d-karpov <77918194+d-karpov@users.noreply.github.com>
2024-04-23 02:49:42 +0300

    Добавлена логика работы счетчика и отображения истории

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+//MARK: - IB Outlets
+	@IBOutlet weak var countLabel: UILabel!
+	@IBOutlet weak var historyText: UITextView!
+	
+//MARK: - Private Properties
+	private var count: Int = 0
+	private var currentDateTime: String {
+		Date.now.formatted(date: .numeric, time: .shortened)
+	}
+	private enum Operation: String {
+		case start = "История изменений:\n"
+		case add = "значение изменено на +1\n"
+		case sub = "значение изменено на -1\n"
+		case clear = "значение сброшено\n"
+		case error = "попытка уменьшить значение счётчика ниже 0\n"
+	}
+	
+// MARK: - Life Cycle Methods
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		updateHistory(for: .start)
 	}
-
-
+	
+//MARK: - IB Actions
+	@IBAction func addDidTap(_ sender: Any) {
+		updateCount(by: 1)
+		updateHistory(for: .add)
+	}
+	
+	@IBAction func subDidTap(_ sender: Any) {
+		if count > 0 {
+			updateCount(by: -1)
+			updateHistory(for: .sub)
+		} else {
+			updateHistory(for: .error)
+		}
+	}
+	
+	@IBAction func clearDidTap(_ sender: Any) {
+		count = 0
+		countLabel.text = "\(count)"
+		updateHistory(for: .clear)
+	}
+	
+//MARK: - Private Methods
+	private func updateCount(by number: Int) {
+		count += number
+		countLabel.text = "\(count)"
+	}
+	
+	private func updateHistory(for operation: Operation) {
+		if operation == .start {
+			historyText.text.append(contentsOf: operation.rawValue)
+		} else {
+			historyText.text.append(contentsOf: "[\(currentDateTime)]: \(operation.rawValue)")
+		}
+		let range = NSRange(location: historyText.text.count - 1, length: 0)
+		historyText.scrollRangeToVisible(range)
+	}
 }
-
