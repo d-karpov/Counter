dd3fa7609213fe0a17293c2b4a46abb85710cc91
d-karpov <77918194+d-karpov@users.noreply.github.com>
2024-04-23 22:50:58 +0300

    Небольшие корректировки после ревью
    Небольшие корректировки после ревью

--- Counter/Presentation/ViewController.swift
+++ Counter/Presentation/ViewController.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 	
-//MARK: - IB Outlets
-	@IBOutlet weak var countLabel: UILabel!
-	@IBOutlet weak var historyText: UITextView!
+	//MARK: - IB Outlets
+	@IBOutlet private weak var countLabel: UILabel!
+	@IBOutlet private weak var historyText: UITextView!
 	
-//MARK: - Private Properties
+	//MARK: - Private Properties
 	private var count: Int = 0
 	private var currentDateTime: String {
 		Date.now.formatted(date: .numeric, time: .standard)
@@ -26,34 +26,13 @@ class ViewController: UIViewController {
 		case error = "попытка уменьшить значение счётчика ниже 0\n"
 	}
 	
-// MARK: - Life Cycle Methods
+	// MARK: - Life Cycle Methods
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		updateHistory(for: .start)
 	}
 	
-//MARK: - IB Actions
-	@IBAction func addDidTap(_ sender: Any) {
-		updateCount(by: 1)
-		updateHistory(for: .add)
-	}
-	
-	@IBAction func subDidTap(_ sender: Any) {
-		if count > 0 {
-			updateCount(by: -1)
-			updateHistory(for: .sub)
-		} else {
-			updateHistory(for: .error)
-		}
-	}
-	
-	@IBAction func clearDidTap(_ sender: Any) {
-		count = 0
-		animatedCountTextUpdate()
-		updateHistory(for: .clear)
-	}
-	
-//MARK: - Private Methods
+	//MARK: - Private Methods
 	private func updateCount(by number: Int) {
 		count += number
 		animatedCountTextUpdate()
@@ -78,4 +57,26 @@ class ViewController: UIViewController {
 			self.countLabel.text = "\(self.count)"
 		}
 	}
+	
+	//MARK: - IB Actions
+	@IBAction private func addDidTap(_ sender: Any) {
+		updateCount(by: 1)
+		updateHistory(for: .add)
+	}
+	
+	@IBAction private func subDidTap(_ sender: Any) {
+		if count > 0 {
+			updateCount(by: -1)
+			updateHistory(for: .sub)
+		} else {
+			updateHistory(for: .error)
+		}
+	}
+	
+	@IBAction private func clearDidTap(_ sender: Any) {
+		count = 0
+		animatedCountTextUpdate()
+		updateHistory(for: .clear)
+	}
+	
 }
